4684b7d15dabc7e82d05425f80f091b59e2f5d88
Rio <onwukachibike@gmail.com>
2023-01-05 17:16:25 +0100

    deployed nftmarketplace contract

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -9,7 +9,13 @@ async function main() {
   const NFT = await ethers.getContractFactory("NFT");
   const nft = await NFT.deploy();
 
+  // deploy contracts here:
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  const marketplace = await Marketplace.deploy(1);
+
   console.log("NFT contract address", nft.address)
+  console.log("Marketplace contract address", marketplace.address)
+
   
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
   saveFrontendFiles(nft, "NFT");
