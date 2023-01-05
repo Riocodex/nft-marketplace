1daca372f245d297fc545e6428357273bb553d35
Rio <onwukachibike@gmail.com>
2023-01-05 17:34:44 +0100

    created test to check name and symbol

--- src/backend/test/NFTMarketplace.test.js
+++ src/backend/test/NFTMarketplace.test.js
@@ -1,1 +1,23 @@
-const
+const { expect } = require("chai")
+
+describe("NFTMarketplace", async function(){
+    let deployer, addr1, addr2, nft, marketplace
+    let feePercent = 1
+    beforeEach(async function() {
+        //get Contract address
+    const NFT = await ethers.getContractFactory("NFT")
+    const Marketplace = await ethers.getContractFactory("Marketplace")
+    //get signers
+    [deployer, addr1, addr2] = await ethers.getSigners()
+    //deploy contracts
+    nft = await NFT.deploy();
+    marketplace = await Marketplace.deploy(feePercent)
+    });
+    describe("Deployment", function(){
+        it("Should track name and symbol of the nft collection", async function(){
+            expect(await nft.name()).to.equal("Rio NFT")
+            expect(await nft.symbol()).to.equal("RIO")
+
+        })
+    })
+})
