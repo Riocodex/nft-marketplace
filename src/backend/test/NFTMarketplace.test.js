9a132741af169c60b328014fb358e9adc77f4b0c
Rio <onwukachibike@gmail.com>
2023-01-05 17:42:22 +0100

    created test to check name and symbol

--- src/backend/test/NFTMarketplace.test.js
+++ src/backend/test/NFTMarketplace.test.js
@@ -1,23 +1,43 @@
-const { expect } = require("chai")
+const { expect } = require("chai"); 
 
-describe("NFTMarketplace", async function(){
-    let deployer, addr1, addr2, nft, marketplace
-    let feePercent = 1
-    beforeEach(async function() {
-        //get Contract address
-    const NFT = await ethers.getContractFactory("NFT")
-    const Marketplace = await ethers.getContractFactory("Marketplace")
-    //get signers
-    [deployer, addr1, addr2] = await ethers.getSigners()
-    //deploy contracts
+const toWei = (num) => ethers.utils.parseEther(num.toString())
+const fromWei = (num) => ethers.utils.formatEther(num)
+
+describe("NFTMarketplace", function () {
+
+  let NFT;
+  let nft;
+  let Marketplace;
+  let marketplace
+  let deployer;
+  let addr1;
+  let addr2;
+  let addrs;
+  let feePercent = 1;
+  let URI = "sample URI"
+
+  beforeEach(async function () {
+    // Get the ContractFactories and Signers here.
+    NFT = await ethers.getContractFactory("NFT");
+    Marketplace = await ethers.getContractFactory("Marketplace");
+    [deployer, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    // To deploy our contracts
     nft = await NFT.deploy();
-    marketplace = await Marketplace.deploy(feePercent)
-    });
-    describe("Deployment", function(){
-        it("Should track name and symbol of the nft collection", async function(){
-            expect(await nft.name()).to.equal("Rio NFT")
-            expect(await nft.symbol()).to.equal("RIO")
+    marketplace = await Marketplace.deploy(feePercent);
+  });
 
-        })
-    })
+  describe("Deployment", function () {
+
+    it("Should track name and symbol of the nft collection", async function () {
+      // This test expects the owner variable stored in the contract to be equal
+      // to our Signer's owner.
+      const nftName = "Rio NFT"
+      const nftSymbol = "RIO"
+      expect(await nft.name()).to.equal(nftName);
+      expect(await nft.symbol()).to.equal(nftSymbol);
+    });
+
+    
+  });
 })
