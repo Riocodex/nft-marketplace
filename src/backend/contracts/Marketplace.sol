fb297a17c58d8c2ea55628d224d9b3c0b2eabb2c
Rio <onwukachibike@gmail.com>
2023-01-05 18:13:24 +0100

    created makeitem function

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -11,8 +11,39 @@ contract Marketplace is ReentrancyGuard {
     uint public immutable feePercent; //the fee percentage on files
     uint public itemCount;
 
+    struct item{
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+
+    
+
+    //itemId => item
+    mapping(uint => Item) public items;
+
     constructor(uint _feePercent){
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant{
+        require(_price > 0, "Price must be greater than zero");
+        //increment itemCount
+        itemCount ++;
+        //transfer nft to contract
+        _nft.transferFrom(msg.sender, address(this), _tokenId)
+        //add nft to item mapping
+        items[itemCount] = item {
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false 
+        }
     }
 }
