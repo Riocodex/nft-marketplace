377ab29a46882b942d0a67b0a4222952ce43b16e
Rio <onwukachibike@gmail.com>
2023-01-05 16:56:25 +0100

    created nftmarkeplace contract

---
+++ src/backend/contracts/Marketplace.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Marketplace is ReentrancyGuard {
+    //stateVariables
+}
