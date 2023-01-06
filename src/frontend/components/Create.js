eefe613a3a0ec39e153da39af8121141bfde2ef5
Rio <onwukachibike@gmail.com>
2023-01-06 14:21:25 +0100

    updated form

--- src/frontend/components/Create.js
+++ src/frontend/components/Create.js
@@ -61,9 +61,27 @@ const Create = () => {
               name="file"
               onChange={uploadToIPFS}
             />
-            <Form.Control onChange={(e) => setName(e.target.value)} size="lg" required type="text" placeholder="Name" />
-            <Form.Control onChange={(e) => setDescription(e.target.value)} size="lg" required as="textarea" placeholder="Description" />
-            <Form.Control onChange={(e) => setPrice(e.target.value)} size="lg" required type="number" placeholder="Price in ETH" />
+            <Form.Control 
+            onChange={(e) => setName(e.target.value)} 
+            size="lg" 
+            required type="text" 
+            placeholder="Name" 
+            />
+
+            <Form.Control 
+            onChange={(e) => setDescription(e.target.value)}
+            size="lg" 
+            required as="textarea"
+            placeholder="Description" 
+            />
+
+            <Form.Control 
+            onChange={(e) => setPrice(e.target.value)} 
+            size="lg" 
+            required type="number" 
+            placeholder="Price in 
+            ETH" 
+            />
             <div className="d-grid px-0">
               <Button onClick={createNFT} variant="primary" size="lg">
                 Create & List NFT!
