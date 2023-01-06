c99226d59fc28651b5d73487e137fe5ef78c757c
Rio <onwukachibike@gmail.com>
2023-01-06 13:29:19 +0100

    updated the home component

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -54,7 +54,7 @@ function App() {
 
   return (
     <BrowserRouter>
-     <div>
+     <div className="App">
      <Navigation web3Handler={web3Handler} account={account}/>
      {loading ? (
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
@@ -77,33 +77,6 @@ function App() {
               } />
             </Routes>
           )}
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo"/>
-              </a>
-              <h1 className= "mt-5">Dapp University Starter Kit</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-              </a>
-            </div>
-          </main>
-        </div>
-      </div>
     </div>
     </BrowserRouter>
   );
