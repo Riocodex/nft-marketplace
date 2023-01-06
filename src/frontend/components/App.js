31c76e60a74fc3fdbb45b8080c0031f843fbeeae
Rio <onwukachibike@gmail.com>
2023-01-06 12:20:15 +0100

    updated frontend
    added linking

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,21 +1,30 @@
+//tools
+import logo from './logo.png';
+import './App.css';
+import { Spinner } from 'react-bootstrap'
+
+//packages
+import { useState } from 'react'
+import { ethers } from  "ethers"
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
 
-import logo from './logo.png';
-import './App.css';
 
-import { useState } from 'react'
-import { ethers } from  "ethers"
-
+//data
 import MarketplaceAbi from '../contractsData/Marketplace.json'
 import MarketplaceAddress from '../contractsData/Marketplace-address.json'
 import NFTAbi from '../contractsData/NFT.json'
 import NFTAddress from '../contractsData/NFT-address.json'
 
+//files
 import Navigation from './Navbar'
+import Home from './Home.js'
+import Create from './Create.js'
+import MyListedItems from './MyListedItems.js'
+import MyPurchases from './MyPurchases.js'
 
 
  
@@ -47,9 +56,27 @@ function App() {
     <BrowserRouter>
      <div>
      <Navigation web3Handler={web3Handler} account={account}/>
-     <Routes>
-      
-     </Routes>
+     {loading ? (
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh' }}>
+              <Spinner animation="border" style={{ display: 'flex' }} />
+              <p className='mx-3 my-0'>Awaiting Metamask Connection...</p>
+            </div>
+          ) : (
+            <Routes>
+              <Route path="/" element={
+                <Home marketplace={marketplace} nft={nft} />
+              } />
+              <Route path="/create" element={
+                <Create marketplace={marketplace} nft={nft} />
+              } />
+              <Route path="/my-listed-items" element={
+                <MyListedItems marketplace={marketplace} nft={nft} account={account} />
+              } />
+              <Route path="/my-purchases" element={
+                <MyPurchases marketplace={marketplace} nft={nft} account={account} />
+              } />
+            </Routes>
+          )}
       <div className="container-fluid mt-5">
         <div className="row">
           <main role="main" className="col-lg-12 d-flex text-center">
