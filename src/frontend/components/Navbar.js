9cf0707af267df9e4cacc228be36fd27603152ed
Rio <onwukachibike@gmail.com>
2023-01-06 12:33:06 +0100

    updated

--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -2,16 +2,16 @@ import {
     Link
 } from "react-router-dom";
 import { Navbar, Nav, Button, Container } from 'react-bootstrap'
-// import market from './market.png'
+import myLogo from './my-logo.png'
 
 const Navigation = ({ web3Handler, account }) => {
     return (
         <Navbar expand="lg" bg="secondary" variant="dark">
             <Container>
-                {/* <Navbar.Brand href="http://www.dappuniversity.com/bootcamp">
-                    <img src={market} width="40" height="40" className="" alt="" />
-                    &nbsp; DApp NFT Marketplace
-                </Navbar.Brand> */}
+                <Navbar.Brand href="https://riomax.netlify.app/">
+                    <img src={myLogo} width="40" height="40" className="" alt="" />
+                    &nbsp;  NFT Marketplace
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
