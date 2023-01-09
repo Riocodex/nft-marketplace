ec4e376e78eaac52e77715a981257020ba99eabf
Rio <onwukachibike@gmail.com>
2023-01-09 12:20:40 +0100

    updated mypurchases.js

--- src/frontend/components/MyPurchases.js
+++ src/frontend/components/MyPurchases.js
@@ -5,6 +5,46 @@ import { Row, Col, Card } from 'react-bootstrap'
 
 
 const MyPurchases = ({ marketplace, nft, account }) => {
+    const [loading, setLoading] = useState(true)
+    const [purchases, setPurchases] = useState([])
+    const loadPurchasedItems = async () => {
+      // Fetch purchased items from marketplace by quering Offered events with the buyer set as the user
+      const filter =  marketplace.filters.Bought(null,null,null,null,null,account)
+      const results = await marketplace.queryFilter(filter)
+      //Fetch metadata of each nft and add that to listedItem object.
+      const purchases = await Promise.all(results.map(async i => {
+        // fetch arguments from each result
+        i = i.args
+        // get uri url from nft contract
+        const uri = await nft.tokenURI(i.tokenId)
+        // use uri to fetch the nft metadata stored on ipfs 
+        const response = await fetch(uri)
+        const metadata = await response.json()
+        // get total price of item (item price + fee)
+        const totalPrice = await marketplace.getTotalPrice(i.itemId)
+        // define listed item object
+        let purchasedItem = {
+          totalPrice,
+          price: i.price,
+          itemId: i.itemId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image
+        }
+        return purchasedItem
+      }))
+      setLoading(false)
+      setPurchases(purchases)
+    }
+    useEffect(() => {
+      loadPurchasedItems()
+    }, [])
+    if (loading) return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Loading...</h2>
+      </main>
+    )
+  
   return (
     <div>
       
