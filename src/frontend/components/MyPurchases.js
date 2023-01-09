c4e877924c81577144053196bfe837a03a686d3a
Rio <onwukachibike@gmail.com>
2023-01-09 11:55:18 +0100

    updated

--- src/frontend/components/MyPurchases.js
+++ src/frontend/components/MyPurchases.js
@@ -1,3 +1,15 @@
 import { useState, useEffect } from 'react'
 import { ethers } from "ethers"
 import { Row, Col, Card } from 'react-bootstrap'
+
+
+
+const MyPurchases = ({ marketplace, nft, account }) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MyPurchases
